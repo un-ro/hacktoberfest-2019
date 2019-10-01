32ff4c02c9d68ec07fb7236e73f9fab5535c1581
Zainul Muhaimin <zainmuhaimin01@gmail.com>
2019-10-01 23:00:56 +0700

    Some Update

--- netijen/znmn/Webpage With Responsive Design/halo.php
+++ netijen/znmn/Webpage With Responsive Design/halo.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Halo HTML</title>
+	<title><?php echo "HALO HTML"; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link rel="icon" href="assets/img/icon.jpg" type="image/jpg" sizes="16x16"> 
