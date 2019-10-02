1e68c33aa95ecdebe0dfef214e1fa77badd0ab5d
ctrndk <ctrndk@gmail.com>
2019-10-03 02:51:47 +0700

    Adding info on config.php

--- netijen/ctrndk/app/config.php
+++ netijen/ctrndk/app/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '');//KALAU MAU UPLOAD HOSTING KOSONGI AJA YA
 define('DB_DATABASE', 'alumni');
 $koneksi = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 $database = mysqli_select_db($koneksi, DB_DATABASE);
