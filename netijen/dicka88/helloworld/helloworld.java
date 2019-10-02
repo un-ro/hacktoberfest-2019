af8200fffa588df889078d7a872ba49995a27ac4
Dicka Ismaji <dickaismaji@gmail.com>
2019-10-02 21:41:29 +0700

    Create helloworld.java

---
+++ netijen/dicka88/helloworld/helloworld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        int age = 17;
+        if(age >= 17){
+            System.out.println("Hello World Teen");
+        }else{
+            System.out.println("Hello World Children");
+        }
+    }
+}
