7334336d5933c5e9142be8748236c01c1609b99a
gunawan <gunawankurniyaaji>
2019-10-05 11:13:46 +0700

    ngupdet trosss

---
+++ netijen/gunawankurniyaaji/praktiksastu.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+    printf("Fakultas Sains dan Teknologi\n");
+    printf("Gunawan Kurniya Aji\n");
+    printf("2120180152\n");
+    printf("Prodi Teknik Informatika\n");
+}
