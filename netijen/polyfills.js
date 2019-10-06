c6902fb5671b8be5c8d1bb16c63fa9c8ffea1941
gevakun <35886384+gevakun@users.noreply.github.com>
2019-10-06 18:59:32 +0700

    Delete polyfills.js

--- netijen/polyfills.js
+++
@@ -1,9 +0,0 @@
-/**
- * Add startsWith support for IE
- */
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function (searchString, position) {
-        position = position || 0;
-        return this.indexOf(searchString, position) === position;
-    };
-}
